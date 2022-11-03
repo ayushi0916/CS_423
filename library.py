4ad787d256530cee3c8bd343fd231f3c6d76fcf4
UO-CIS211 <86554954+ayushi0916@users.noreply.github.com>
2022-11-02 23:49:57 -0700

    Update library.py
    Added 3 functions to my library.

--- library.py
+++ library.py
@@ -238,3 +238,70 @@ def find_random_state(features_df, labels, n=200):
   rs_value = sum(var)/len(var)  #get average ratio value
   idx = np.array(abs(var - rs_value)).argmin()
   return idx
+
+
+titanic_transformer = Pipeline(steps=[
+    ('drop', DropColumnsTransformer(['Age', 'Gender', 'Class', 'Joined', 'Married',  'Fare'], 'keep')),
+    ('gender', MappingTransformer('Gender', {'Male': 0, 'Female': 1})),
+    ('class', MappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
+    ('ohe', OHETransformer(target_column='Joined')),
+    ('age', TukeyTransformer(target_column='Age', fence='outer')), #from chapter 4
+    ('fare', TukeyTransformer(target_column='Fare', fence='outer')), #from chapter 4
+    ('minmax', MinMaxTransformer()),  #from chapter 5
+    ('imputer', KNNTransformer())  #from chapter 6
+    ], verbose=True)
+
+
+customer_transformer = Pipeline(steps=[
+    ('id', DropColumnsTransformer(column_list=['ID'])),
+    ('os', OHETransformer(target_column='OS')),
+    ('isp', OHETransformer(target_column='ISP')),
+    ('level', MappingTransformer('Experience Level', {'low': 0, 'medium': 1, 'high':2})),
+    ('gender', MappingTransformer('Gender', {'Male': 0, 'Female': 1})),
+    ('time spent', TukeyTransformer('Time Spent', 'inner')),
+    ('minmax', MinMaxTransformer()),
+    ('imputer', KNNTransformer())
+    ], verbose=True)
+
+
+def dataset_setup(full_table, label_column_name:str, the_transformer, rs, ts=.2):
+  #your code below
+  titanic_features = full_table.drop(columns=label_column_name)
+  labels = full_table[label_column_name].to_list()
+  X_train, X_test, y_train, y_test = train_test_split(titanic_features, labels, test_size=ts, shuffle=True,
+                                                    random_state=rs, stratify=labels)
+  X_train_transformed = titanic_transformer.fit_transform(X_train)
+  X_test_transformed = titanic_transformer.fit_transform(X_test)
+  x_trained_numpy = X_train_transformed.to_numpy()
+  x_test_numpy = X_test_transformed.to_numpy()
+  y_train_numpy = np.array(y_train)
+  y_test_numpy = np.array(y_test)
+
+  return x_trained_numpy, x_test_numpy, y_train_numpy,  y_test_numpy
+
+def titanic_setup(titanic_table, transformer=titanic_transformer, rs=40, ts=.2):
+  X_train, X_test, y_train, y_test = train_test_split(titanic_table, labels, test_size=ts, shuffle=True,
+                                                    random_state=rs, stratify=labels)
+  X_train_transformed = titanic_transformer.fit_transform(X_train)
+  X_test_transformed = titanic_transformer.fit_transform(X_test)
+  x_trained_numpy = X_train_transformed.to_numpy()
+  x_test_numpy = X_test_transformed.to_numpy()
+  y_train_numpy = np.array(y_train)
+  y_test_numpy = np.array(y_test)
+
+  return x_trained_numpy, x_test_numpy, y_train_numpy,  y_test_numpy
+
+def customer_setup(customer_table, transformer=customer_transformer, rs=76, ts=.2):
+  customers_features = customer_table.drop(columns=['Rating'])
+  labels = customer_table['Rating'].to_list()
+  X_train, X_test, y_train, y_test = train_test_split(customers_features, labels, test_size=ts, shuffle=True,
+                                                    random_state=rs, stratify=labels)
+                                                    
+  X_train_transformed = customer_transformer.fit_transform(X_train)
+  X_test_transformed = customer_transformer.fit_transform(X_test)
+  x_trained_numpy = X_train_transformed.to_numpy()
+  x_test_numpy = X_test_transformed.to_numpy()
+  y_train_numpy = np.array(y_train)
+  y_test_numpy = np.array(y_test)
+
+  return x_trained_numpy, x_test_numpy, y_train_numpy,  y_test_numpy
