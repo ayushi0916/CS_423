8a8692cd3a9d38806bd162ed5047f485d5a7fc41
UO-CIS211 <86554954+ayushi0916@users.noreply.github.com>
2022-11-30 21:45:18 -0800

    added logreg lib

--- library.py
+++ library.py
@@ -5,6 +5,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegressionCV
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import HalvingGridSearchCV
