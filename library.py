43e810fc0df8b8d583cf9ad5ff66aab67ede953f
UO-CIS211 <86554954+ayushi0916@users.noreply.github.com>
2022-12-03 00:14:02 -0800

    Update library.py

--- library.py
+++ library.py
@@ -9,6 +9,7 @@ from sklearn.linear_model import LogisticRegressionCV
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import HalvingGridSearchCV
+from xgboost import XGBClassifier
 
 class MappingTransformer(BaseEstimator, TransformerMixin):
   
